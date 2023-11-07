6620226132b523cf600da16f2ea14fdb640f6780
Damian Mroczek <damianmroczek@yahoo.com>
2023-11-07 16:44:56 +0700

    expenseFilter component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -22,22 +22,25 @@ const ExpenseForm = (props) => {
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
-            date: new Date(enteredDate)
+            date: new Date(enteredDate),
         };
 
         props.onSaveExpenseData(expenseData);
-        setEnteredTitle('');
-        setEnteredAmout('');
-        setEnteredDate('');
+        setEnteredTitle("");
+        setEnteredAmout("");
+        setEnteredDate("");
     };
 
     return (
-
         <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+                    <input
+                        type="text"
+                        value={enteredTitle}
+                        onChange={titleChangeHandler}
+                    />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,15 +1,14 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 const NewExpense = (props) => {
-
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
-            id: Math.random().toString()
+            id: Math.random().toString(),
         };
 
-        props.onAddExpense(expenseData)
-    }
+        props.onAddExpense(expenseData);
+    };
 
     return (
         <div className="new-expense">
